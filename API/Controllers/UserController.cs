d1fa2c950c80080e93db39573154225dc6f3b409
meg <megfa@yahoo.com>
2023-08-09 21:01:33 +0200

    sec 4

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -1,14 +1,12 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-[ApiController]
-[Route("api/[controller]")] // /api/users
-
-public class UsersController : ControllerBase
+public class UsersController : BaseApiController
 {
     private readonly DataContext _context;
 
@@ -23,6 +21,7 @@ public class UsersController : ControllerBase
         return await _context.Users.ToListAsync();
     }
 
+    [Authorize]
     [HttpGet("{id}")] //api/users/3
     public async Task<ActionResult<AppUser>> GetUser(int id)
     {
